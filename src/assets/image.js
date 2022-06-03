3e6dfdaab25024ffdbbce67fbeccd27a6337e483
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-06-03 21:13:20 +0700

    commit

--- src/assets/image.js
+++
@@ -1,9 +0,0 @@
-import HeroBanner from './hero_banner.svg';
-import RmitLogo from './rmit-logo.png';
-
-const IllustrationResource = {
-  HeroBanner,
-  RmitLogo,
-};
-
-export default IllustrationResource;
--- src/pages/Landing.jsx
+++
@@ -1,7 +0,0 @@
-import Hero from '../component/landing/hero/Hero';
-
-const Landing = () => {
-  return <Hero />;
-};
-
-export default Landing;
